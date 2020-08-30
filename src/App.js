cb4d99aaee6bb69de0c92ff7e45dfeb26c39f1b2
prince <princeakpabio90@gmail.com>
2020-08-30 19:18:24 +0100

    created sign-up feature using firebase snapshot and refrence objects, also set up auth state for sign-in feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import './App.css';
-import LandingPage from './pages/landing-page/landing-page.component';
+import React from "react";
+import { Route } from "react-router-dom";
+import LandingPage from "./pages/landing-page/landing-page.component";
 
-function App() {
-  return (
-    <div className="netflix-app container-fluid">
-      <Route exact path='/landing' component={LandingPage} />
-    </div>
-  );
+import "./App.css";
+
+import {
+  auth,
+  createUserProfileDocument,
+} from "../src/firebase/firebase.utilis";
+import SignUpPage from "./pages/sign-up-page/sign-up-page.component";
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="netflix-app container-fluid">
+        <Route exact path="/landing" component={LandingPage} />
+        <Route exact path="/signup" component={SignUpPage} />
+      </div>
+    );
+  }
 }
 
 export default App;
